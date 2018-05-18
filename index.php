b6fbe4ec9cd32c9d2244ec25ef04a7f31edcd036
Pascal CANTAT <pcantat@gmail.com>
2018-05-18 09:42:05 +0200

    première version de mon site

---
+++ index.php
@@ -0,0 +1,14 @@
+//  https://gitforwindows.org/
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mon Site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <p>Hello !!</p>
+    </body>
+</html>
